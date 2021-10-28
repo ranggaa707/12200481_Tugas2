607a2cd83eb362e41eeb3dc4cd76720db20e5700
ranggaa707 <ranggaalam021@gmail.com>
2021-10-28 13:47:38 +0700

    Tugas pertemuan 2 mengerjakan contoh1.php

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+ public function index()
+ {
+    echo "<h1>Perkenalkan</h1>";
+    echo"Nama saya Rangga Anugerah Putra Saya tinggal di Depok olah raga yang saya sukai adalah Bulu tangkis";
+ } 
+}
+?>
